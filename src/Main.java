01246cfb08c2f36a3e6cbd600e5ae02807629404
bahtiyarkalayci <49812375+bahtiyarkalayci@users.noreply.github.com>
2022-11-18 22:05:01 +0300

    https://www.patika.dev/tr

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
+import javax.sound.midi.SysexMessage;
+import java.util.Scanner;
+
 public class Main {
+    static boolean isPalidrom(int number){
+        int temp=number,lastNumber,reverseNumber=0;
+         while(temp!=0) {
+             lastNumber = temp % 10;
+             reverseNumber = (reverseNumber * 10) + lastNumber;
+             temp /= 10;
+         }
+if (reverseNumber==temp)
+    return true;
+else
+    return false;
+
+    }
+
+
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("enter number");
+        int num =scanner.nextInt();
+        isPalidrom(num);
     }
 }
